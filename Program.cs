e24087716db6c16da9fbbdc780d6ce0822d04358
Eeewing09 <semenkan2009@gmail.com>
2026-09-11 09:29:30 +0300

    Lab03: increment and decrement

--- Program.cs
+++ Program.cs
@@ -9,3 +9,18 @@ balance *= 1.05; // начисление 5 %
 Console.WriteLine($"После начисления 5%: {balance}");
 balance /= 2; // раздели счет пополам с партнером
 Console.WriteLine($"После деления пополам: {balance}");
+Console.WriteLine();
+Console.WriteLine("Постфикс vs префикс");
+int lessonNumber = 1;
+Console.WriteLine($"lessonNubmer++ выводит:{lessonNumber++}");
+Console.WriteLine($"после этого lessonNubmer выводит:{lessonNumber}");
+int weekNumber = 1;
+
+Console.WriteLine($"++weekNumber выводит: {++weekNumber}");
+Console.WriteLine($"После этого weekNumber выводит: {weekNumber}");
+Console.WriteLine();
+Console.WriteLine("Практическая ловушка");
+int attempts = 0;
+Console.WriteLine($"Попытка №{++attempts}");
+Console.WriteLine($"Попытка №{++attempts}");
+Console.WriteLine($"Всего попыток: {attempts}");
